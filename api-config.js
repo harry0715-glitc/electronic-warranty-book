290747a71eb6504e282ea15ed68e99c0954ced7e
harry0715-glitc <harry0715-glitc@users.noreply.github.com>
2026-06-13 19:01:26 +0800

    Set live Apps Script web app endpoint

--- api-config.js
+++ api-config.js
@@ -1,5 +1,5 @@
 window.APP_CONFIG = {
-  apiBase: "",
+  apiBase: "https://script.google.com/macros/s/AKfycbxhiL2RwaD6yOlQJVb8MQwJW6zuz0rvcNQdIiCmtwM7JvlWqWzIvaSiGFF6fWXJbw9NYA/exec",
   publicBase: "https://harry0715-glitc.github.io/electronic-warranty-book",
   liffId: "2010316548-ci9nYsou",
   companyName: "楓根室內裝修設計有限公司",
