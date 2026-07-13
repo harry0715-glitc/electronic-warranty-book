93edea19559995bcb29f34f853be5ae25a686010
harry0715-glitc <harry0715-glitc@users.noreply.github.com>
2026-07-13 10:37:01 +0800

    Add GitHub Pages admin shell

--- docs/api-config.js
+++ docs/api-config.js
@@ -1,5 +1,7 @@
 window.APP_CONFIG = {
   apiBase: "https://script.google.com/macros/s/AKfycbyIq4j6NPL_xMSoPfIY6kzzswQxRUXErgffBNgIen-xoU8OxaBC3etJtY82DXroSOtZ4Q/exec",
+  adminApiBase: "https://script.google.com/macros/s/AKfycbxK3U_BbLdI335S6vUn_RX93_CzNARuj4upTDVkwySJBtfpNVqNnriKQZm91qJrf1QsIw/exec",
+  useGoogleSessionAuth: false,
   publicBase: "https://harry0715-glitc.github.io/electronic-warranty-book",
   liffId: "2010316548-KZgCnSKp",
   companyName: "楓根室內裝修設計有限公司",
